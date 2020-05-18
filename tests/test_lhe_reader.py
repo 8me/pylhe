6366e6e2403e69266f699c086fdf4171f044fe1c
Johannes Schumann <johannes.schumann@fau.de>
2020-05-18 17:16:02 +0200

    Added tests and test data

--- tests/test_lhe_reader.py
+++ tests/test_lhe_reader.py
@@ -1,24 +1,38 @@
-import pytest
+#!/usr/bin/env python
+# coding=utf-8
+# Filename: test_lhe_reader.py
+
+__author__ = "Johannes Schumann"
+__copyright__ = "Copyright 2020, Johannes Schumann"
+__credits__ = []
+__license__ = "MIT"
+__maintainer__ = "Johannes Schumann"
+__email__ = "jschumann@km3net.de"
+__status__ = "Development"
+
+import unittest
 import pylhe
-import skhep_testdata
+from os.path import dirname, join
 
-TEST_FILE = skhep_testdata.data_path("pylhe-testfile-pr29.lhe")
+TEST_DATA_DIR = join(dirname(__file__), 'test_data')
 
+class TestLHEFile(unittest.TestCase):
+    def setUp(self):
+        self.test_file = join(TEST_DATA_DIR, 'EventOutput.Pert.00000001.lhe')
 
-def test_event_count():
-    assert pylhe.readNumEvents(TEST_FILE) == 791
+    def test_event_count(self):
+        assert pylhe.readNumEvents(self.test_file) == 791
 
-
-def test_lhe_init():
-    init_data = pylhe.readLHEInit(TEST_FILE)
-    init_info = init_data["initInfo"]
-    assert init_info["beamA"] == pytest.approx(1.0)
-    assert init_info["beamB"] == pytest.approx(2.0)
-    assert init_info["energyA"] == pytest.approx(1.234567)
-    assert init_info["energyB"] == pytest.approx(2.345678)
-    assert init_info["PDFgroupA"] == pytest.approx(3.0)
-    assert init_info["PDFgroupB"] == pytest.approx(4.0)
-    assert init_info["PDFsetA"] == pytest.approx(5.0)
-    assert init_info["PDFsetB"] == pytest.approx(6.0)
-    assert init_info["weightingStrategy"] == pytest.approx(7.0)
-    assert init_info["numProcesses"] == pytest.approx(8.0)
+    def test_lhe_init(self):
+        init_data = pylhe.readLHEInit(self.test_file)
+        init_info = init_data['initInfo']
+        self.assertAlmostEqual(init_info['beamA'], 1.0)
+        self.assertAlmostEqual(init_info['beamB'], 2.0)
+        self.assertAlmostEqual(init_info['energyA'], 1.234567)
+        self.assertAlmostEqual(init_info['energyB'], 2.345678)
+        self.assertAlmostEqual(init_info['PDFgroupA'], 3.0)
+        self.assertAlmostEqual(init_info['PDFgroupB'], 4.0)
+        self.assertAlmostEqual(init_info['PDFsetA'], 5.0)
+        self.assertAlmostEqual(init_info['PDFsetB'], 6.0)
+        self.assertAlmostEqual(init_info['weightingStrategy'], 7.0)
+        self.assertAlmostEqual(init_info['numProcesses'], 8.0)
