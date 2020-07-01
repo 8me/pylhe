d5e74fb572f07e1745d2f202e25e5efc79763f94
Johannes Schumann <johannes.schumann@fau.de>
2020-07-02 01:03:17 +0200

    Require testdata version >=0.3.1

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup
 
 extras_require = {
-    "develop": [
+    "test": [
+        "pytest",
+        "pytest-cov>=2.5.1",
+        "scikit-hep-testdata>=0.3.1",
+        "pydocstyle",
         "check-manifest",
-        "pyflakes",
-        "pre-commit",
-        "black;python_version>='3.6'",  # Black is Python3 only
-        "twine",
+        "flake8",
     ],
 }
+extras_require["lint"] = sorted(set(["pyflakes", "black;python_version>='3.6'"]))
+extras_require["develop"] = sorted(
+    set(extras_require["test"] + ["pre-commit", "check-manifest", "twine"])
+)
 extras_require["complete"] = sorted(set(sum(extras_require.values(), [])))
 
 setup(
